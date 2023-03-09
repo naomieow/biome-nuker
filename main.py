3d29fae4c21506708848e0c019007e78523c77f6
naomi <naomieow@proton.me>
2023-03-09 16:17:58 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
+import logging
 from nbt import nbt
 
-def nuke_biomes(nuked_namespace: str, dimension: str , file_path: str = "./level.dat") -> None:
-    nbtfile = nbt.NBTFile(file_path, "rb")
+def nuke_biomes(nuked_namespace: str, dimension: str , file: str | nbt.NBTFile = "./level.dat") -> nbt.NBTFile:
+    if isinstance(file, str):
+        logging.info(f"Getting NBTFile from '{file}'")
+        nbtfile = nbt.NBTFile(file, "rb")
+    elif isinstance(file, nbt.NBTFile):
+        logging.info("NBTFile supplied")
+        nbtfile = file
+    else:
+        raise TypeError(f"Expected type of 'str' or 'nbt.NBTFile', found '{type(file)}'")
+
     biomes_location = nbtfile["Data"]["WorldGenSettings"]["dimensions"][dimension]["generator"]["biome_source"]["biomes"]
     for index, tag in enumerate(biomes_location):
         biome = str(tag['biome']).split(":")
+        logging.debug(f"Checking biome {biome}")
         if biome[0] == nuked_namespace:
+            logging.debug(f"Matched biome {biome} to {nuked_namespace}. Nuking..")
             biomes_location[index].clear()
-            nbtfile.write_file("export.dat")
-            # print(biomes_location[index])
-            # help(nbt.TAG_Compound)
-        # print(type(tag))
 
-nuke_biomes("incendium", "minecraft:the_nether")
+    return nbtfile
+
+if __name__ == "__main__":
+    nuke_biomes("incendium", "minecraft:the_nether").write_file("export.dat")
